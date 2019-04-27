2e52a08a91269da01831a7bfa542fd576a1d1b0b
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2019-04-27 17:39:34 -0500

    first commit

---
+++ com/test/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    Main();
+    public static void main (String [] args){
+        Scanner entrada = new Scanner(System.in);
+        int s = entrada.nextInt();
+        System.out.print();
+    }
+}
